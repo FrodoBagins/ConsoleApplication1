e2de21ee9b697a5df0adc1558717e6733c63e904
FrodoBagins <martinez199509@gmail.com>
2016-10-09 23:31:49 +0200

    first commit

---
+++ ConsoleApplication1/PersonDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    using System.Data.Entity;
+
+    /// <summary>
+    /// The person db context.
+    /// </summary>
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext() : base("name=DataModelContext")
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PerformDatabaseOperations();
+            Console.Write("Person saved !");
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        ///     The perform database operations.
+        /// </summary>
+        public static void PerformDatabaseOperations()
+        {
+            using (var db = new PersonDbContext())
+            {
+                var person = new Person
+                {
+                    FirstName = "Nadege",
+                    LastName = "Deroussen",
+                    BirthDate = DateTime.Now
+                };
+
+                db.Persons.Add(person);
+                db.SaveChanges();
+            }
+        }
+    }
+}
